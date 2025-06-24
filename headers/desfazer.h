6884e4e7067dfed935880b87c403d289556f260c
Geovana Santos <geovanasousafs2@hotmail.com>
2025-06-24 16:42:09 -0300

    criar desfazer.h com a estrutura da pilha de desfazer

--- headers/desfazer.h
+++ headers/desfazer.h
@@ -1,8 +1,17 @@
-//
-// Created by kawandias on 17/06/25.
-//
-
 #ifndef DESFAZER_H
 #define DESFAZER_H
 
-#endif //DESFAZER_H
+typedef struct NoPilha {
+    char tipo[20]; // "atividade" ou "participante"
+    char nome[100];
+    char infoExtra[100]; // Pode ser matrícula ou horário
+    struct NoPilha *prox;
+} NoPilha;
+
+// Funções da Pilha de Desfazer
+void inicializarPilha(NoPilha **topo);
+void empilhar(NoPilha **topo, char tipo[], char nome[], char infoExtra[]);
+void desempilhar(NoPilha **topo);
+void listarPilha(NoPilha *topo);
+
+#endif
