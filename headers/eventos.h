f642dabf211985320ecb716badf7984b05d7b841
MinAny7 <marianygomes2014@gmail.com>
2025-06-27 22:11:16 -0300

    Adiciona arquivo header de eventos

--- headers/eventos.h
+++ headers/eventos.h
@@ -5,6 +5,22 @@
 #ifndef EVENTOS_H
 #define EVENTOS_H
 
+#define MAX_NOME 100
+#define MAX_DATA 11
+
+struct Atividade;
+
+typedef struct Evento{
+    char nome[MAX_NOME];
+    char data[MAX_DATA];
+    struct Evento *prox;
+    struct Atividade *atividades;
+}Evento;
+
 void saudacao();
+
+void cadastrar_evento();
+void listar_eventos();
+void remover_evento();
 
 #endif // EVENTOS_H
