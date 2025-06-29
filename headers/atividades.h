84cc6fd47f1f0426949c88012105ff5a843ffbba
simao-morais <simao.morais@ufpi.edu.br>
2025-06-29 00:08:59 -0300

    adiconado funcoes de atividades

--- headers/atividades.h
+++ headers/atividades.h
@@ -1,7 +1,3 @@
-//
-// Created by kawandias on 17/06/25.
-//
-
 #ifndef ATIVIDADES_H
 #define ATIVIDADES_H
 
@@ -10,9 +6,17 @@
 typedef struct Atividade {
     char titulo[100];
     char horario[6];
+    int  hora;     
     Participante *participantes; // lista duplamente encadeada
     struct Atividade *prox;
 } Atividade;
 
+// FUNCOES DE ATIVIDADES
+Atividade *criarAtividade(char titulo[], int hora);
+void inserirAtividade(Atividade **lista, Atividade *nova);
+void listarAtividades(Atividade *lista);
+void removerAtividade(Atividade **lista, char titulo[]);
+void ordenarAtividades(Atividade **lista);
+int verificarConflito(Atividade *lista, int hora);
 
 #endif //ATIVIDADES_H
